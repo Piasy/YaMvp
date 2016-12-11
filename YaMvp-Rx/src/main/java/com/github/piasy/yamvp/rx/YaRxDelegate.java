ac8ce5d2361cf26c7633a5b13d7bbec44229d6f0
Piasy <xz4215@gmail.com>
2016-12-11 21:17:44 +0800

    remove rxjava 1.x integration

--- YaMvp-Rx/src/main/java/com/github/piasy/yamvp/rx/YaRxDelegate.java
+++ YaMvp-Rx/src/main/java/com/github/piasy/yamvp/rx/YaRxDelegate.java
@@ -3,8 +3,6 @@ package com.github.piasy.yamvp.rx;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import javax.annotation.concurrent.ThreadSafe;
-import rx.Subscription;
-import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by Piasy{github.com/Piasy} on 17/09/2016.
@@ -12,41 +10,8 @@ import rx.subscriptions.CompositeSubscription;
 
 @ThreadSafe
 public class YaRxDelegate {
-    private CompositeSubscription mSubscriptions2Stop;
-    private CompositeSubscription mSubscriptions2Destroy;
-
     private CompositeDisposable mDisposables2Stop;
     private CompositeDisposable mDisposables2Destroy;
-
-    public synchronized boolean addUtilStop(Subscription subscription) {
-        if (mSubscriptions2Stop == null) {
-            throw new IllegalStateException(
-                    "addUtilStop should be called between onStart and onStop");
-        }
-        mSubscriptions2Stop.add(subscription);
-        return true;
-    }
-
-    public synchronized boolean addUtilDestroy(Subscription subscription) {
-        if (mSubscriptions2Destroy == null) {
-            throw new IllegalStateException(
-                    "addUtilDestroy should be called between onCreate and onDestroy");
-        }
-        mSubscriptions2Destroy.add(subscription);
-        return true;
-    }
-
-    public synchronized void remove(Subscription subscription) {
-        if (mSubscriptions2Stop == null && mSubscriptions2Destroy == null) {
-            throw new IllegalStateException("remove should not be called after onDestroy");
-        }
-        if (mSubscriptions2Stop != null) {
-            mSubscriptions2Stop.remove(subscription);
-        }
-        if (mSubscriptions2Destroy != null) {
-            mSubscriptions2Destroy.remove(subscription);
-        }
-    }
 
     public synchronized boolean addUtilStop(Disposable disposable) {
         if (mDisposables2Stop == null) {
@@ -79,11 +44,6 @@ public class YaRxDelegate {
     }
 
     public synchronized void onCreate() {
-        if (mSubscriptions2Destroy != null) {
-            throw new IllegalStateException("onCreate called multiple times");
-        }
-        mSubscriptions2Destroy = new CompositeSubscription();
-
         if (mDisposables2Destroy != null) {
             throw new IllegalStateException("onCreate called multiple times");
         }
@@ -91,11 +51,6 @@ public class YaRxDelegate {
     }
 
     public synchronized void onStart() {
-        if (mSubscriptions2Stop != null) {
-            throw new IllegalStateException("onStart called multiple times");
-        }
-        mSubscriptions2Stop = new CompositeSubscription();
-
         if (mDisposables2Stop != null) {
             throw new IllegalStateException("onStart called multiple times");
         }
@@ -103,12 +58,6 @@ public class YaRxDelegate {
     }
 
     public synchronized void onStop() {
-        if (mSubscriptions2Stop == null) {
-            throw new IllegalStateException("onStop called multiple times or onStart not called");
-        }
-        mSubscriptions2Stop.unsubscribe();
-        mSubscriptions2Stop = null;
-
         if (mDisposables2Stop == null) {
             throw new IllegalStateException("onStop called multiple times or onStart not called");
         }
@@ -117,13 +66,6 @@ public class YaRxDelegate {
     }
 
     public synchronized void onDestroy() {
-        if (mSubscriptions2Destroy == null) {
-            throw new IllegalStateException(
-                    "onDestroy called multiple times or onCreate not called");
-        }
-        mSubscriptions2Destroy.unsubscribe();
-        mSubscriptions2Destroy = null;
-
         if (mDisposables2Destroy == null) {
             throw new IllegalStateException(
                     "onDestroy called multiple times or onCreate not called");
--- YaMvp-Rx/src/main/java/com/github/piasy/yamvp/rx/YaRxPresenter.java
+++ YaMvp-Rx/src/main/java/com/github/piasy/yamvp/rx/YaRxPresenter.java
@@ -3,7 +3,6 @@ package com.github.piasy.yamvp.rx;
 import com.github.piasy.yamvp.YaPresenter;
 import com.github.piasy.yamvp.YaView;
 import io.reactivex.disposables.Disposable;
-import rx.Subscription;
 
 /**
  * Created by Piasy{github.com/Piasy} on 17/09/2016.
@@ -16,18 +15,6 @@ public abstract class YaRxPresenter<V extends YaView> extends YaPresenter<V> {
     protected YaRxPresenter() {
         mYaRxDelegate = new YaRxDelegate();
         mYaRxDelegate.onCreate();
-    }
-
-    protected boolean addUtilStop(Subscription subscription) {
-        return mYaRxDelegate.addUtilStop(subscription);
-    }
-
-    public boolean addUtilDestroy(Subscription subscription) {
-        return mYaRxDelegate.addUtilDestroy(subscription);
-    }
-
-    public void remove(Subscription subscription) {
-        mYaRxDelegate.remove(subscription);
     }
 
     protected boolean addUtilStop(Disposable disposable) {
